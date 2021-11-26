ea78f578d46e19e2f22729c91b10aa4fc06467cf
Judtsandy <91551677+Judtsandy@users.noreply.github.com>
2021-11-25 21:15:21 -0600

    Add files via upload

---
+++ EjemploComposicion.m
@@ -0,0 +1,28 @@
+% octave.script.2.7_EjemploComposicion 
+% Escuela:               Tecnologico de Estudios Superiores de Jilotepec
+% Carrera:               Ingenieria en Sistemas Computacionales
+% Titulo:                Operaciones con funciones: adicción, multiplicación,
+%                        división y composición.
+% Descripcion:           Ejemplos de operaciones en las funciones
+% Autor:                 Equipo Muerte Matematica
+% Fecha:                 25 de Noviembre del 2021
+% Version:               1
+% Notas:                 Requiere symbolic
+% 2_7_EjemploComposicion
+
+%Limpiar varibles 
+clear 
+
+%Activar el paquete symbolic
+pkg load symbolic
+syms x
+
+%Funciones
+f(x)=((-x.^2)+5);
+g(x)=((2*x)+3);
+
+fgx= g(f(x));
+
+%Plotear funcion
+ezplot(fgx);
+grid on;
